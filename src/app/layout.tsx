e767edcfb4b2b47bfd6617fcd2fe01295344ae7a
dudumarcosgamer <00001107141680sp@al.educacao.sp.gov.br>
2025-11-14 15:42:19 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Criado com a Lasy",
+  title: "Criado pelo Borges&Oliveira",
   description: "Projeto criado com Lasy AI",
 };
 
